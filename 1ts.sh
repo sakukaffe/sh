8a8ab62cddc7f90164915ef6a917075dea5806dd
sakukaffe <57263265+sakukaffe@users.noreply.github.com>
2022-11-25 22:36:26 +0100

    Update 1ts.sh

--- 1ts.sh
+++ 1ts.sh
@@ -1,4 +1,4 @@
-mkdir -p /home/ts
+mkdir -p /home/ts321
 wget -O /home/ts321/ts.zip https://github.com/sakukaffe/sh/raw/main/ts.zip
 chmod 0777 /home/ts321/*
 ./home/ts321/ts3server_startscript.sh start
