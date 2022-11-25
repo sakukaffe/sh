d0a58e9de832de4b16bcfafd7d079f5cd4239253
sakukaffe <57263265+sakukaffe@users.noreply.github.com>
2022-11-25 22:14:11 +0100

    Create ts.sh

---
+++ ts.sh
@@ -0,0 +1,8 @@
+mdir -p /home/ts
+cd /home/ts
+wget https://files.teamspeak-services.com/releases/server/3.13.7/teamspeak3-server_linux_amd64-3.13.7.tar.bz2
+tar xf teamspeak3-server_linux_amd64-3.13.7.tar.bz2
+cd teamspeak3-server_linux_amd64
+mv * ..
+cd ..
+./ts3server_startscript.sh
