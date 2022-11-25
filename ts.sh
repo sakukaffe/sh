9031a9ccd5198f4619dec712b804f065e8ca6a63
sakukaffe <57263265+sakukaffe@users.noreply.github.com>
2022-11-25 22:53:05 +0100

    Update and rename 1ts.sh to ts.sh

--- ts.sh
+++ ts.sh
@@ -1,5 +1,6 @@
 mkdir -p /home/ts321
 wget -O /home/ts321/ts.zip https://github.com/sakukaffe/sh/raw/main/ts.zip
-unzip /home/ts321/ts.zip
+unzip /home/ts321/ts.zip /home/ts321
+rm /home/ts321/ts.zip
 chmod 0777 /home/ts321/*
 ./../home/ts321/ts3server_startscript.sh start
