46c2ecce7590a629b269f0daffd946bf0e7036b1
sakukaffe <57263265+sakukaffe@users.noreply.github.com>
2022-11-25 22:34:36 +0100

    Update 1ts.sh

--- 1ts.sh
+++ 1ts.sh
@@ -1,7 +1,4 @@
 mkdir -p /home/ts
-cd /home/ts
-wget -o /home/ts https://files.teamspeak-services.com/releases/server/3.13.7/teamspeak3-server_linux_amd64-3.13.7.tar.bz2
-
-tar xf teamspeak3-server_linux_amd64-3.13.7.tar.bz2
-cd teamspeak3-server_linux_amd64
-./ts3server_startscript.sh start
+wget -O /home/ts321/ts.zip https://files.teamspeak-services.com/releases/server/3.13.7/teamspeak3-server_linux_amd64-3.13.7.tar.bz2
+chmod 0777 /home/ts321/*
+./home/ts321/ts3server_startscript.sh start
